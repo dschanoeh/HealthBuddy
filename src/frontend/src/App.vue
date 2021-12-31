8061e39d741c830de134684f648e272e7a26c3c4
Jan-Niklas Meier <jan@jansblog.org>
2021-12-31 15:02:31 +0100

    Adding footer

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <Dashboard />
+  <b-container>
+    <Dashboard />
+    <Footer />
+  </b-container>
 </template>
 
 <script>
 import Dashboard from "./components/Dashboard.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   name: "App",
   components: {
     Dashboard,
+    Footer,
   },
   created() {
     document.title = "HealthBuddy";
---
+++ src/frontend/src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+    <footer class="m-5">
+        <center>
+            <a href="https://github.com/dschanoeh/HealthBuddy" class="text-muted">Healthbuddy</a>
+        </center>
+    </footer>
+</template>
